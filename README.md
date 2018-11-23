c24f093d818f518787e2eef6b3e2d5209b257a7e
lungern <xiii.at.cn@gmail.com>
2018-11-23 17:44:10 +0800

    add src install script

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ This is a website based on nodejs and react. Its purpose is simple: Show your th
 * [nodejs](https://github.com/nodejs/node)
 * [react](https://github.com/facebook/react)
 * [koa](https://github.com/koajs/koa)
-* [next]()
+* [next.js](https://github.com/zeit/next.js)
 * web assembly
 * webGL
 <!-- more to do  -->
---
+++ assets/files/src/install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+echo download nodejs
+git clone https://github.com/nodejs/node.git
+echo download react
+git clone https://github.com/facebook/react.git
+echo download koa
+git clone https://github.com/koajs/koa.git
+echo download next.js
+git clone https://github.com/zeit/next.js.git
+echo download vue
+git clone https://github.com/vuejs/vue.git
+echo download successfully
